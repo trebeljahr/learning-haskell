94f9e16a0d043b16bf81174f526a11544cb2b509
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-18 09:35:53 +0200

    Move import examples into own folder

--- example.hs
+++ example.hs
@@ -1,29 +1,3 @@
--- using import statements
-import Data.List
--- import only specific functions
-import Data.Bool (otherwise, not)
--- import everything except specific function
-import Data.String hiding (words)
--- import with prefix use - to prevent name clashes
-import qualified Data.String
-import qualified Data.List.NonEmpty as DLN 
-
--- now functions from Data.list like nub are accessible. 
-removeDuplicates :: (Eq a) => [a] -> [a]
-removeDuplicates = nub
-
--- use words function from string library. Notice the prefix.
-countWords :: String -> Int
-countWords = length . Data.String.words
-
--- the other functions of the string library were imported 
--- without prefix and can be used without it, like lines below.
-countLines :: String -> Int 
-countLines = length . lines 
-
--- using shortened qualified import syntax.
-intersperse' = DLN.intersperse 
-
 main :: IO ()
 main = putStrLn "Hello, World"
 
