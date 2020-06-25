ceeecbd827875ba5764a373cd4b0e5e7ed17c16a
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-25 12:06:41 +0200

    Complete types chapter

--- typeclasses.hs
+++ typeclasses.hs
@@ -60,3 +60,16 @@ instance YesNo KittensAndDogs where
 instance Functor B.Tree where  
     fmap f B.EmptyTree = B.EmptyTree  
     fmap f (B.Node x leftsub rightsub) = B.Node (f x) (fmap f leftsub) (fmap f rightsub)  
+
+class Tofu t where  
+    tofu :: j a -> t a j 
+
+data Frank a b  = Frank {frankField :: b a} deriving (Show)  
+
+instance Tofu Frank where 
+    tofu x = Frank x 
+
+data Barry t k p = Barry { yabba :: p, dabba :: t k }  
+
+instance Functor (Barry a b) where  
+    fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba = y}  
