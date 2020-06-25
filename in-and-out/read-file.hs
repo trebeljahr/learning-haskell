73bf4b094da4c49a15e512f698adf688f3705d50
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-25 22:04:48 +0200

    Add read-file example

---
+++ in-and-out/read-file.hs
@@ -0,0 +1,9 @@
+import System.IO  
+  
+main = do  
+    putStr "Please enter the location you want to open: "
+    filePath <- getLine
+    handle <- openFile filePath ReadMode  
+    contents <- hGetContents handle  
+    putStr contents  
+    hClose handle  
