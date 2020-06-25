fa898e40a9386a786ce1a14fe857c5b84518c890
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-25 21:36:45 +0200

    Add more examples

---
+++ in-and-out/long-lines.hs
@@ -0,0 +1,10 @@
+main = do 
+    contents <- getContents
+    putStr $ longLines contents 
+
+longLines :: String -> String
+longLines input = 
+    let inputLines = lines input 
+        longLines = filter (\line -> length line > 10) inputLines
+    in unlines longLines 
+
