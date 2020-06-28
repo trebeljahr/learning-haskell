3142dcc653ad2ba444ba54fba183c943e9156242
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-28 21:49:35 +0200

    Add monad examples

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# I am learning HASKELL! 
-### This is a small repo, where I play around with a bunch of haskell learning the concepts. 
-### In it I will follow the amazing and FREE book learnyouahaskell. You should check it out since it is epic.  http://learnyouahaskell.com/chapters
-I have heard that haskell is one of the languages that really force... ahm... teach you to write functional programs. And I want to learn that style of programming. I think that it will make me a better devoper. And I like that thought.  
-## Some cool resources I also found: 
+# I am learning HASKELL!
+
+### This is a small repo, where I play around with a bunch of haskell learning the concepts.
+
+### In it I will follow the amazing and FREE book learnyouahaskell. You should check it out since it is epic. http://learnyouahaskell.com/chapters
+
+I have heard that haskell is one of the languages that really force... ahm... teach you to write functional programs. And I want to learn that style of programming. I think that it will make me a better devoper. And I like that thought.
+
+## Some cool resources I also found:
+
 1. ofc the official site: https://www.haskell.org/
 1. and the haskell wiki: https://wiki.haskell.org/Haskell
 1. https://docs.haskellstack.org/en/stable/README/
@@ -11,4 +16,4 @@ I have heard that haskell is one of the languages that really force... ahm... te
 1. http://www.happylearnhaskelltutorial.com/
 1. http://book.realworldhaskell.org/read/
 1. https://github.com/chrisdone/hindent
-1. https://acm.wustl.edu/functional/haskell.php 
+1. https://acm.wustl.edu/functional/haskell.php
---
+++ monads.hs
@@ -0,0 +1,72 @@
+applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b  
+applyMaybe Nothing f  = Nothing  
+applyMaybe (Just x) f = f x  
+
+decideOver x = if x > 2 then Just x else Nothing
+x = Just 3 `applyMaybe` decideOver
+-- x ==> Just 3
+y = Just 1 `applyMaybe` decideOver
+-- y ==> Nothing
+z = Nothing `applyMaybe` decideOver
+-- z ==> Nothing 
+
+
+-- the Monad Class 
+-- class Monad m where  
+--     return :: a -> m a  
+  
+--     (>>=) :: m a -> (a -> m b) -> m b  
+  
+--     (>>) :: m a -> m b -> m b  
+--     x >> y = x >>= \_ -> y  
+  
+--     fail :: String -> m a  
+--     fail msg = error msg  
+
+type Birds = Int  
+type Pole = (Birds,Birds)  
+
+-- defining the infix syntax 
+x -: f = f x 
+
+landLeft :: Birds -> Pole -> Maybe Pole  
+landLeft n (left,right) 
+ | abs ((left + n) - right) < 4 = Just (left + n, right)
+ | otherwise = Nothing 
+  
+landRight :: Birds -> Pole -> Maybe Pole  
+landRight n (left,right) 
+ | abs (left - (right + n))  < 4 = Just (left, right + n)
+ | otherwise = Nothing 
+
+
+-- return (0,0) >>= landRight 2 >>= landLeft 2 >>= landRight 2
+-- ==> Just (2,4)
+
+banana :: Pole -> Maybe Pole  
+banana _ = Nothing  
+
+-- return (0,0) >>= landLeft 1 >>= banana >>= landRight 1
+-- ==> Nothing 
+-- which is the same as 
+-- return (0,0) >>= landLeft 1 >> Nothing >>= landRight 1
+-- ==> Nothing 
+
+lameDoStuff :: Maybe String 
+lameDoStuff = Just "The sum is " >>= (\a -> 
+              Just 5 >>= (\b ->
+              Just 10 >>= (\c -> 
+              Just "!" >>= (\d -> 
+                  Just (a ++ (show $ b + c) ++ d)
+                  )))) 
+
+-- everything inside do is a monad -> mind blown.
+-- lameDoStuff and epicDoStuff are functionally equivalent. 
+-- the epic version is just so much cleaner.
+epicDoStuff :: Maybe String 
+epicDoStuff = do 
+    a <- Just "The sum is "
+    b <- Just 5
+    c <- Just 10
+    d <- Just "!"
+    Just (a ++ (show $ b + c) ++ d)
